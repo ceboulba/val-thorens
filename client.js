b39ee30f6a58b73840873f8221162d0c4c497e22
ceboulba <antoine@archipicture.fr>
2018-08-18 23:47:01 +0200

    first commit

---
+++ client.js
@@ -0,0 +1,22 @@
+import {ReactInstance} from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    fullScreen: true,
+    ...options,
+  });
+
+  r360.renderToSurface(
+    r360.createRoot('SlideshowSample', {
+      photos: [
+        {uri: './static_assets/val-thorens-entree.jpg', title: 'Accueil', format: '2D'},
+        {uri: './static_assets/val-thorens-hall.jpg', title: 'Hall', format: '2D'},
+        // Add your own 180 / 360 photos to this array,
+        // with an associated title and format
+      ],
+    }),
+    r360.getDefaultSurface(),
+  );
+}
+
+window.React360 = {init};
